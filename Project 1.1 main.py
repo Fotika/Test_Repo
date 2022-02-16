7ee031b409beb503ab33a2372c56d69dc333fd8b
Favour Otika <99708984+Fotika@users.noreply.github.com>
2022-02-16 09:57:03 -0800

    Add files via upload

---
+++ Project 1.1 main.py
@@ -0,0 +1,10 @@
+# OTIKA FAVOUR
+# 005159016
+
+# Calculation for converting Celsius to Fahrenheit
+def Celsius_to_Fahrenheit(Celsius):
+    Fahrenheit = ((9/5)* Celsius + 32)
+# Calculation for converting  Fahrenheit to Celsius
+def Fahrenheit_to_Celsius(Fahrenheit):
+    Celsius = (Fahrenheit - 32) * 5/9
+print("This app converts Temperature Celcisu/Fahrenheit")
